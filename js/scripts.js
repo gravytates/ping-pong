300a9949c218b333290784694f200c6927f2bf31
Grady L Shelton <gradyish@gmail.com>
2017-03-31 15:58:01 -0700

    reevaluate strategy for negative integers. succeed in implementing negative value results with custom ping pong multiples. use regex in isNum function to make a different data manipulation branch with altered strings to reflect negative counting

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,50 @@
 var isNum = function(input) {
   if (input.match(/^\d+$/)){
     return makeArray(input);
-  }
+  } else if (input.match(/^-?\d+$/)) {
+    return (negMakeArray(input));
+  } else {
     return false;
+  }
 }
 
+// NEGATIVE INTEGERS
+var negMakeArray = function(end) {
+  var negFirstArray = []
+  for(i=-1; i >= end; i--){
+    negFirstArray.push(i);
+  }
+  return negPingPongHunter(negFirstArray);
+}
+
+var negPingPongHunter = function(negFirstArray) {
+  var negPingPongArray = []
+  for(i=0; i <= negFirstArray.length - 1; i++) {
+    var negNewNums = (negFirstArray[i]);
+    if (negFirstArray[i] % 15 === 0){
+      negPingPongArray.push("negative ping-pong!");
+    } else if (negFirstArray[i] % 5 === 0){
+      negPingPongArray.push("negative pong!");
+    } else if (negFirstArray[i] % 3 === 0){
+      negPingPongArray.push("negative ping!");
+    } else {
+      negPingPongArray.push(negNewNums);
+    }
+  }
+  return negMakeList(negPingPongArray);
+}
+
+var negMakeList = function(negPingPongArray) {
+  var negList = document.createElement('ul');
+  for(var i=0; i < negPingPongArray.length; i++){
+  var negItem = document.createElement('li');
+  negItem.appendChild(document.createTextNode(negPingPongArray[i]));
+  negList.appendChild(negItem);
+  }
+  return negList;
+}
+
+// POSITIVE INTEGERS
 var makeArray = function(end) {
   var firstArray = []
   for(i=1; i <= end; i++){
